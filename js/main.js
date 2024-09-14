72c0ca0049c85dfa73615e90dce9992051025e20
Pedro Henrique <ps9517130@gmail.com>
2024-09-14 18:57:48 -0300

    feat: add the style and solve the overlapping bug

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
 let selectCidades = document.querySelector('#cidades');
 let listaCidades = [];
 
-// Variáveis de elementos do perfil
-const perfilDiv = document.createElement('div');
-const foto = document.createElement('img');
-const nomeUrna = document.createElement('span');
-const numeroUrna = document.createElement('span');
-const nomeCompleto = document.createElement('span');
-const cidade = document.createElement('span');
-const partido = document.createElement('span');
-const siglaPartido = document.createElement('span');
-const instagramLink = document.createElement('a');
-const instagramImg = document.createElement('img');
-const facebookLink = document.createElement('a');
-const facebookImg = document.createElement('img');
-
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch("./candidatos.json").then((response) => {
@@ -41,20 +27,45 @@ document.addEventListener('DOMContentLoaded', () => {
                     
                 
                 selectCidades.addEventListener('click',() => {
-                    console.log(selectCidades.value);
-                    
+                    let divCandidatos = document.querySelector('.candidatos');
+                            
+                            //Limpando as div para atualizar os dados exibidos
+                            divCandidatos.innerHTML = '';
+
                     dados.candidatos.forEach((candidato) => {
                         if(selectCidades.value == candidato.cidade) {
-                            let divCandidatos = document.querySelector('.candidatos');
+                            
+                         
+                            // Variáveis de elementos do perfil
+                            const perfilDiv = document.createElement('div');
+                            const foto = document.createElement('img');
+                            const nomeUrna = document.createElement('span');
+                            const numeroUrna = document.createElement('span');
+                            const nomeCompleto = document.createElement('span');
+                            const cidade = document.createElement('span');
+                            const partido = document.createElement('span');
+                            const siglaPartido = document.createElement('span');
+                            const instagramLink = document.createElement('a');
+                            const instagramImg = document.createElement('img');
+                            const facebookLink = document.createElement('a');
+                            const facebookImg = document.createElement('img');
 
+                            //Definindo id dos elementos do perfil
+                            instagramImg.id = 'instagram';
+                            facebookImg.id = 'facebook';
+                            nomeUrna.id = 'nomeUrna';
+                            numeroUrna.id = 'numeroUrna';
+                            nomeCompleto.id = 'nomeCompleto';
+                            cidade.id = 'cidade';
+                            partido.id = 'partidoSigla';
+                            
                             // Escrevendo os dados nas variáveis
                             foto.src = candidato.linkFoto;
                             nomeUrna.textContent = candidato.nomeUrna;
                             numeroUrna.textContent = candidato.numUrna;
                             nomeCompleto.textContent = candidato.nomeCompleto;
                             cidade.textContent = candidato.cidade;
-                            partido.textContent = candidato.partido;
-                            siglaPartido.textContent = candidato.siglaPartido;
+                            partido.textContent = `${candidato.partido} - ${candidato.siglaPartido}`;
                             instagramLink.href = candidato.instagram;
                             instagramImg.src = 'svgs/instagram.svg';
                             facebookLink.href = candidato.facebook;
@@ -71,12 +82,12 @@ document.addEventListener('DOMContentLoaded', () => {
                             perfilDiv.appendChild(nomeCompleto);
                             perfilDiv.appendChild(cidade);
                             perfilDiv.appendChild(partido);
-                            perfilDiv.appendChild(siglaPartido);
+
+                            // perfilDiv.appendChild(siglaPartido);
                             perfilDiv.appendChild(instagramLink);
                             perfilDiv.appendChild(facebookLink);
 
                             divCandidatos.appendChild(perfilDiv);
-
                         }
                     })
                 })
